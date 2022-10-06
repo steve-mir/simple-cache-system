6fc6e89bf9ec9b74b3792451bf05f2adb6f97f8e
steve-mir <ekechukwuemeka25@gmail.com>
2022-10-06 14:01:52 +0100

    completed

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+const SIZE = 5
+
 type Node struct {
 	Value string
 	Left  *Node
@@ -39,17 +41,62 @@ func (c *Cache) Check(str string) {
 	node := &Node{}
 
 	if val, ok := c.Hash[str]; ok {
-	node:
-		c.Remove(val)
+		node = c.Remove(val)
 	} else {
 		node = &Node{Value: str}
 	}
+	c.Add(node)
+	c.Hash[str] = node
+}
+
+func (c *Cache) Remove(n *Node) *Node {
+	fmt.Printf("remove: %s\n", n.Value)
+	left := n.Left
+	right := n.Right
+
+	left.Right = right
+	right.Left = left
+	c.Queue.Length -= 1
+	delete(c.Hash, n.Value)
+	return n
+}
+
+func (c *Cache) Add(n *Node) {
+	fmt.Printf("add: %s\n", n.Value)
+	temp := c.Queue.Head.Right
+
+	c.Queue.Head.Right = n
+	n.Left = c.Queue.Head
+	n.Right = temp
+	temp.Left = n
+
+	c.Queue.Length++
+	if c.Queue.Length > SIZE {
+		c.Remove(c.Queue.Tail)
+	}
+}
+
+func (c *Cache) Display() {
+	c.Queue.Display()
+}
+
+func (q *Queue) Display() {
+	node := q.Head.Right
+	fmt.Printf("%d - [", q.Length)
+	for i := 0; i < q.Length; i++ {
+		fmt.Printf("{%s}", node.Value)
+		if i < q.Length-1 {
+			fmt.Printf("<-->")
+		}
+		node = node.Right
+	}
+	fmt.Println("]")
 }
 
 func main() {
 	fmt.Println("START CACHE")
 	cache := NewCache()
-	for _, word := range []string{"parrot", "pear", "mango"} {
+	for _, word := range []string{"lion", "tiger", "AI", "panda", "eagle"} {
 		cache.Check(word)
 		cache.Display()
 	}
